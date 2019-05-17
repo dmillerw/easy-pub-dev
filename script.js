2bdc37f704c91e9a4d38651407cd90244be0bdf8
= <dmillerw@gmail.com>
2019-05-17 02:41:45 -0700

    Done

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 (function () {
     var header = document.querySelector(".package-header");
     var etitle = document.querySelector(".title");
+    var meta = document.querySelector(".metadata");
     var title = etitle
         .textContent
         .split(" ");
     var version = title[0] + ": ^" + title[1];
 
     var button = document.createElement("a");
-    button.setAttribute("class", "package-tag");
+    button.setAttribute("class", "copy-button-norm");
     button.innerHTML = "Copy to Clipboard";
     button.addEventListener("click",function() {
         var tempInput = document.createElement("input");
@@ -17,7 +18,11 @@
         tempInput.select();
         document.execCommand("copy");
         document.body.removeChild(tempInput);
+
+        button.setAttribute("class", "copy-button-copied");
+        button.innerHTML = "Copied!";
     });
 
-    etitle.appendChild(button);
+    var tags = meta.querySelector(".tags");
+    meta.insertBefore(button, tags);
 })();
